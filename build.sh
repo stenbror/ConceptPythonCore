a5e3b839849f88dd41c8d252bb84cccbc2447b3f
Richard Magnor Stenbro <stenbror@hotmail.com>
2020-08-30 01:37:04 +0200

    updated build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 clang++ -o test --std=c++17 -I. main.cc abstractSyntaxTree.cc pythonCoreParser.cc pythonCoreTokenizer.cc
+clang++ -o libPythonCore.so -shared -fPIC --std=c++17 -I. main.cc abstractSyntaxTree.cc pythonCoreParser.cc pythonCoreTokenizer.cc
