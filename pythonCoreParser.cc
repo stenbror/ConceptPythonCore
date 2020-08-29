9739ed6a498a4ac12a07862093d0dee8a764b4ac
Richard Magnor Stenbro <stenbror@hotmail.com>
2020-08-30 00:04:01 +0200

    Template for first expression methods

--- pythonCoreParser.cc
+++ pythonCoreParser.cc
@@ -2,3 +2,23 @@
 
 using namespace PythonCore::Runtime;
 
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseNamedExpr() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseTest() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseTestNoCond() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseLambdaDef() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseLambdaDefNoCond() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseOrTest() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseAndTest() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseNotTest() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseComparison() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseStarExpr() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseExpr() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseXorExpr() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseAndExpr() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseShiftExpr() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseArithExpr() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseTerm() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseFactor() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parsePower() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseAtomExpr() { return nullptr; }
+std::shared_ptr<ASTExpressionNode> PythonCoreParser::parseAtom() { return nullptr; }
--- pythonCoreParser.h
+++ pythonCoreParser.h
@@ -1,3 +1,4 @@
+
 #ifndef PYTHONCORE_PARSER_H
 #define PYTHONCORE_PARSER_H
 
@@ -7,7 +8,31 @@ namespace PythonCore::Runtime
 {
     class PythonCoreParser
     {
+        public:
 
+            std::shared_ptr<ASTExpressionNode> parseNamedExpr();
+            std::shared_ptr<ASTExpressionNode> parseTest();
+            std::shared_ptr<ASTExpressionNode> parseTestNoCond();
+            std::shared_ptr<ASTExpressionNode> parseLambdaDef();
+            std::shared_ptr<ASTExpressionNode> parseLambdaDefNoCond();
+            std::shared_ptr<ASTExpressionNode> parseOrTest();
+            std::shared_ptr<ASTExpressionNode> parseAndTest();
+            std::shared_ptr<ASTExpressionNode> parseNotTest();
+            std::shared_ptr<ASTExpressionNode> parseComparison();
+            std::shared_ptr<ASTExpressionNode> parseStarExpr();
+            std::shared_ptr<ASTExpressionNode> parseExpr();
+            std::shared_ptr<ASTExpressionNode> parseXorExpr();
+            std::shared_ptr<ASTExpressionNode> parseAndExpr();
+            std::shared_ptr<ASTExpressionNode> parseShiftExpr();
+            std::shared_ptr<ASTExpressionNode> parseArithExpr();
+            std::shared_ptr<ASTExpressionNode> parseTerm();
+            std::shared_ptr<ASTExpressionNode> parseFactor();
+            std::shared_ptr<ASTExpressionNode> parsePower();
+            std::shared_ptr<ASTExpressionNode> parseAtomExpr();
+            std::shared_ptr<ASTExpressionNode> parseAtom();
+
+        protected:
+            std::shared_ptr<Token> m_CurSymbol;
     };
 } // namespace PythonCore::Runtime
 
