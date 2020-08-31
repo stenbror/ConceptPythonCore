caa2843f4881f14c51320f7501d02c83ba216bff
Richard Magnor Stenbro <stenbrorhotmail.com>
2020-08-31 15:21:26 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-clang++ -o test --std=c++17 -I. main.cc abstractSyntaxTree.cc pythonCoreParser.cc pythonCoreTokenizer.cc
-clang++ -o libPythonCore.so -shared -fPIC --std=c++17 -I. main.cc abstractSyntaxTree.cc pythonCoreParser.cc pythonCoreTokenizer.cc
+clang++ -O3 -o test --std=c++17 -I. main.cc abstractSyntaxTree.cc pythonCoreParser.cc pythonCoreTokenizer.cc
+clang++ -O3 -o libPythonCore.so -shared -fPIC --std=c++17 -I. main.cc abstractSyntaxTree.cc pythonCoreParser.cc pythonCoreTokenizer.cc
+strip test
+strip libPythonCore.so
